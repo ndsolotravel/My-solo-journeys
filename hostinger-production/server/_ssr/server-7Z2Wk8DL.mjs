d6b1f9aa6932062c7e94591418c26906c5ae18ee
Hussain <ndsolotravel@gmail.com>
2026-09-14 01:08:43 +0500

    security: upgrade js-yaml to 4.3.2 to resolve vulnerability GHSA-2883-xcg3-v3hh

--- hostinger-production/server/_ssr/server-7Z2Wk8DL.mjs
+++ hostinger-production/server/_ssr/server-7Z2Wk8DL.mjs
@@ -135,7 +135,7 @@ const manifest = {
   },
   "1e527b6631307654bc7f81cb758bd7a871fe81f3441e69d22bb529f27b5e4965": {
     functionName: "listDestinations_createServerFn_handler",
-    importer: () => import("./destinations.functions-BLz2UNI2.mjs")
+    importer: () => import("./destinations.functions-DdawuZkg.mjs")
   },
   "22dbf5a0bd1aa4105a7bf3eff794bd8738e2b5fef5f61aec5d79f152ea65a351": {
     functionName: "adminDeleteDestination_createServerFn_handler",
@@ -187,7 +187,7 @@ const manifest = {
   },
   "42d8c5a0f2ac4a51b2ee36862863046dc42ef21708bdbe47e53e1eb60378f141": {
     functionName: "getDestinationBySlug_createServerFn_handler",
-    importer: () => import("./destinations.functions-BLz2UNI2.mjs")
+    importer: () => import("./destinations.functions-DdawuZkg.mjs")
   },
   "453b18f745aa96dd18df7e1f0f8568922912665dd1f16e9349f48327f9a275eb": {
     functionName: "adminGetSettings_createServerFn_handler",
@@ -327,7 +327,7 @@ const manifest = {
   },
   "9af9309080664fa919d9f0ccc1d1b1233ba78f5ec575fcd3aaab715c0a8a455b": {
     functionName: "listGallery_createServerFn_handler",
-    importer: () => import("./gallery.functions-B703AaSF.mjs")
+    importer: () => import("./gallery.functions-D7xaI057.mjs")
   },
   "9c8084edff95c284c741e3785ab938a71693bfe26e2c07e4c0272b47d311afc2": {
     functionName: "getPostBySlug_createServerFn_handler",
@@ -1700,7 +1700,7 @@ var getBaseManifest = getProdBaseManifest;
 var createEarlyHintsForRequest = createEarlyHintsCollector;
 async function loadEntries() {
   const [routerEntry, startEntry, pluginAdapters] = await Promise.all([
-    import("./router-Jb-EBDhz.mjs").then((n) => n.ak),
+    import("./router-BSNco56j.mjs").then((n) => n.ak),
     import("./start-ClOJrOKy.mjs"),
     import("./empty-plugin-adapters-BFgPZ6_d.mjs")
   ]);
